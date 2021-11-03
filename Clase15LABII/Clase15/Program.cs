66947593bd641df5d119b45deb30c8228ec16443
Marina Fosch <81588438+Marinadfr@users.noreply.github.com>
2021-11-03 16:15:55 -0300

    Add files via upload

---
+++ Clase15LABII/Clase15/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Clase15
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Lavadora myLavadora = new Lavadora();
+            Television myTelevision = new Television();
+            int opcion;
+
+            Console.WriteLine("---------------------------")
+            Console.WriteLine("Elija el producto: ");
+            Console.WriteLine("Televisor [1]");
+            Console.WriteLine("Lavadora [2]");
+            Console.WriteLine("---------------------------")
+            opcion = Convert.ToInt32(Console.ReadLine());
+
+            switch (opcion)
+            {
+                case 1:
+                    myTelevision.precioFinal();
+                    break;
+                case 2:
+                    myLavadora.precioFinal();
+                    break;
+                default:
+                    break;
+            }
+            
+
+
+        }
+    }
+}
